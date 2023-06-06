8af655824955a1339e58e92810f789eadb6a424e
Luis Mendez <81998928+LuisMendez-ING@users.noreply.github.com>
2023-06-05 21:45:30 -0500

    Cambios

--- src/config/LDAP.js
+++ src/config/LDAP.js
@@ -1,5 +1,5 @@
 const ldapconfig = {
-  url: "ldap://sae-ldap:389",
+  url: "ldap://sae-ldap:389/",
   base: "dc=sae,dc=unal,dc=edu,dc=co",
   dn: "cn=admin,dc=sae,dc=unal,dc=edu,dc=co",
   password: "admin"
