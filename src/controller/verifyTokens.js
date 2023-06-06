d437c9e094154cd9b3a873a4abea23c4c1683500
Luis Mendez <81998928+LuisMendez-ING@users.noreply.github.com>
2023-06-06 06:10:18 -0500

    Cambios

---
+++ src/controller/verifyTokens.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+router.post("/tokensVerify", async (req, res) => {
+  //console.log(req.body);
+  if (req.body.tokenDB && req.body.tokenLocalStorage) {
+    const tokenDb = req.body.tokenDb;
+    const tokenLocalS = req.body.tokenLocalStorage;
+
+    const decodedTokenDB = jwt.decode(tokenDb);
+    const decodedTokenLocalS = jwt.decode(tokenLocalS);
+
+    if (JSON.stringify(decodedTokenDB) === JSON.stringify(decodedTokenLocalS)) {
+      return res.status(200).json({
+        verificacion: true
+      });
+    } else {
+      return res.status(200).json({
+        verificacion: false
+      });
+    }
+  }
+});
