d85352b4f91231f27a86a8cd0b9725f4f48b9f94
Luis Mendez <81998928+LuisMendez-ING@users.noreply.github.com>
2023-06-06 06:42:15 -0500

    Cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const signinApi = require("../controller/sign_in");
 const signoutApi = require("../controller/sign_out");
+const verifytokens = require("../controller/verifyTokens");
 const router = express.Router();
 
 router.use(signinApi);
 router.use(signoutApi);
+router.use(verifytokens);
 
 module.exports = router;
